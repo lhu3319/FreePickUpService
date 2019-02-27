629546a2a98db8be7a387b7d7a8adecefacd9be7
Han <lhu3319@nate.com>
2019-02-27 16:17:44 +0900

    디비 이전 완료!

---
+++ ClassLibrary/Commons.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassLibrary
+{
+    class Commons
+    {
+        public Panel getPanel(Hashtable hashtable)
+        {
+            Panel panel = new Panel();
+            panel.Size = (Size)hashtable["size"];
+            panel.Location = (Point)hashtable["point"];
+            panel.BackColor = (Color)hashtable["color"];
+            panel.Name = hashtable["name"].ToString();
+            return panel;//
+        }
+
+        public Button getButton(Hashtable hashtable)
+        {
+            Button btn = new Button();
+            btn.Size = (Size)hashtable["size"];
+            btn.Location = (Point)hashtable["point"];
+            btn.BackColor = (Color)hashtable["color"];
+            btn.Name = hashtable["name"].ToString();
+            btn.Text = hashtable["text"].ToString();
+            btn.Click += (EventHandler)hashtable["click"];
+            return btn;
+        }
+
+        public Label getLabel(Hashtable hashtable)
+        {
+            Label label = new Label();
+            label.Location = (Point)hashtable["point"];
+            label.BackColor = (Color)hashtable["color"];
+            label.Name = hashtable["name"].ToString();
+            label.Text = hashtable["text"].ToString();
+            return label;
+        }
+        public RichTextBox GetRichTextBox(Hashtable hashtable)
+        {
+            RichTextBox richtextBox = new RichTextBox();
+            richtextBox.Name = hashtable["name"].ToString();
+            richtextBox.Size = (Size)hashtable["size"];
+            richtextBox.Font = new Font("Verdana", 10.5f, FontStyle.Italic);
+            richtextBox.Location = (Point)hashtable["point"];
+            return richtextBox;
+        }
+        public TextBox GetTextBox(Hashtable hashtable)
+        {
+            TextBox textBox = new TextBox();
+            textBox.Name = hashtable["name"].ToString();
+            textBox.Width = Convert.ToInt32(hashtable["width"].ToString());
+            textBox.Location = (Point)hashtable["point"];
+            return textBox;
+        }
+        public DateTimePicker GetDateTimePicker(Hashtable hashtable)
+        {
+            DateTimePicker dateTimePicker = new DateTimePicker();
+            dateTimePicker.Name = hashtable["name"].ToString();
+            dateTimePicker.Size = (Size)hashtable["size"];
+            dateTimePicker.Location = (Point)hashtable["point"];
+            dateTimePicker.CustomFormat = "yyyy-MM-dd dddd";
+            return dateTimePicker;
+        }
+        public ComboBox getComboBox(Hashtable hashtable)
+        {
+            ComboBox comboBox = new ComboBox();
+            comboBox.Width = Convert.ToInt32(hashtable["width"].ToString());
+            comboBox.DropDownWidth = Convert.ToInt32(hashtable["width"].ToString());
+            comboBox.Location = (Point)hashtable["point"];
+            comboBox.BackColor = (Color)hashtable["color"];
+            comboBox.Name = hashtable["name"].ToString();
+            comboBox.DisplayMember = "value";
+            comboBox.ValueMember = "Key";
+            return comboBox;
+        }
+
+        public ListView GetListView(Hashtable hashtable)
+        {
+            ListView listView = new ListView();
+            listView.Dock = DockStyle.Fill;
+            listView.View = View.Details;
+            listView.GridLines = true;
+            listView.FullRowSelect = true;
+            listView.BackColor = (Color)hashtable["color"];
+            listView.Name = hashtable["name"].ToString();
+            listView.MouseClick += (MouseEventHandler)hashtable["click"];
+            return listView;
+        }
+    }
+}
---
+++ ClassLibrary/Database.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace ClassLibrary
+{
+    public class Database
+    {
+        MySqlConnection conn;
+        private MySqlConnection connection;
+        private bool status;
+        public Database()
+        {
+            status = Connection();
+        }
+        private bool Connection()
+        {
+            try
+            {
+                conn = new MySqlConnection();
+                string host = "192.168.3.151";
+                string port = "3306";
+                string user = "root";
+                string pwd = "1234";
+                string db = "Project";
+                conn.ConnectionString = string.Format(@"server={0};port={4};user={1};password={2};database={3};convert zero datetime=True", host, user, pwd, db, port);
+
+                conn.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public void Close()
+        {
+            if (status)
+            {
+                conn.Close();
+            }
+        }
+
+        public MySqlDataReader Reader(string sql)
+        {
+            if (status)
+            {
+                try
+                {
+                    MySqlCommand comm = new MySqlCommand();
+                    comm.CommandText = sql;
+                    comm.Connection = conn;
+                    comm.CommandType = CommandType.StoredProcedure;
+                    return comm.ExecuteReader();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public MySqlDataReader Reader2(string sql, Hashtable ht)
+        {
+            if (status)
+            {
+                try
+                {
+                    MySqlCommand comm = new MySqlCommand();
+                    comm.CommandText = sql;
+                    comm.Connection = conn;
+                    comm.CommandType = CommandType.StoredProcedure;
+
+                    foreach (DictionaryEntry data in ht)
+                    {
+                        comm.Parameters.AddWithValue(data.Key.ToString(), data.Value);
+                    }
+
+                    return comm.ExecuteReader();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool ReaderClose(MySqlDataReader sdr)
+        {
+            try
+            {
+                sdr.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool NonQuery2(string sql)
+        {
+            try
+            {
+                if (status)
+                {
+                    MySqlCommand comm = new MySqlCommand();
+                    comm.CommandText = sql;
+                    comm.Connection = conn;
+                    comm.CommandType = CommandType.StoredProcedure;
+                    comm.ExecuteNonQuery();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool NonQuery(string sql, Hashtable ht)
+        {
+            if (status)
+            {
+                try
+                {
+                    MySqlCommand comm = new MySqlCommand();
+                    comm.CommandText = sql;
+                    comm.Connection = conn;
+                    comm.CommandType = CommandType.StoredProcedure;
+
+                    foreach (DictionaryEntry data in ht)
+                    {
+                        comm.Parameters.AddWithValue(data.Key.ToString(), data.Value);
+                    }
+
+                    comm.ExecuteNonQuery();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
